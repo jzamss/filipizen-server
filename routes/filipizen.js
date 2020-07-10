f6028134e290462b540f620311556658c61e5ada
jzamss@gmail.com <jzamss@gmail.com>
2020-07-10 16:57:20 +0800

    service

--- routes/filipizen.js
+++ routes/filipizen.js
@@ -1,52 +1,17 @@
 const ctx = require("../lib/server-context");
 const fetch = require("node-fetch");
+const serviceMgr = require("../lib/service-manager");
 
 const express = require("express");
 const router = express.Router();
 
 router.get("/service/metainfo", async (req, res) => {
-  const { serviceName, connection: connName } = req.query;
-  
   try {
-    const connection = ctx.getConnection(connName);
-    let url = "http://";
-    url += connection["app.host"];
-    url += "/" + (connection["app.cluster"] || "osiris3");
-    url += "/json";
-    url += "/" + (connection["app.context"] || "enterprise");
-    url += "/" + serviceName + ".metaInfo";
-    const retVal = await fetch(url);
-    if (retVal.status !== 200) {
-      throw retVal.statusText;
-    }
-    const sinfo = await retVal.json();
-    let func = "function " + sinfo.serviceName + "(p) {\n";
-    func += "this.proxy = p;\n";
-    const keys = Object.keys(sinfo.methods);
-    for (let i = 0; i < keys.length; i++) {
-      const methodName = keys[i];
-      const method = sinfo.methods[methodName];
-      let args = "";
-      let params = "";
-
-      for (let idx = 0; idx < method.parameters.length; idx++) {
-        args += `p${idx}`;
-        if (idx > 0) params += ", ";
-        params += `p${idx}`;
-      }
-      func += "this." + escapeMethodName(methodName) + "= function(";
-      func += args + (args.length > 0 ? "," : "");
-      func += "handler) {\n";
-      func += 'return this.proxy.invoke("' + methodName + '",';
-      func += "[" + params + "]";
-      func += ", handler );\n";
-      func += "};\n";
-    }
-    func += "}";
-    res.send(func);
-  } catch (err) {
-    console.log(err);
-    res.status(404).send({ error: err.toString() });
+    const { serviceName, connection} = req.query;
+    const svc = await serviceMgr.getService(serviceName, connection);
+    res.send(svc);
+  } catch(err) {
+    res.status(400).send(err.toString());
   }
 });
 
